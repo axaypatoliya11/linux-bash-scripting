e474ad83ddb7b0f156a5aecf0d972169cf414ecb
Axay Patoliya <axay.patoliya@einfochips.com>
2022-02-24 16:27:20 +0530

    added

--- g1-project/find_file.sh
+++ g1-project/find_file.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# reading the path from user
 read -p "enter the path of directory[without /home/] " path
+
+# reading the filename starting characters from user
 read -p "enter the letters consecutively from which you want to find the files " letters
+
+# reading how much days ago last modification done
 read -p "how much days before? " days_before
 
-find $HOME/$path -type f -name "[$letters]*" -mtime -$days_before 
+DAY=$(date -d "$D" '+%d') #current date
+MONTH=$(date -d "$D" '+%b') #current month
+final=$(($DAY-$days_before)) #last modified date = current date - days before last modification done
+
+find $HOME/$path -type f -name "[$letters]*" -ls |grep "$MONTH $final"
